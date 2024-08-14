fb941bebcb126515e6452e14b5f8f94d872560d0
Roquv13 <czarekolszewski99@gmail.com>
2024-08-14 23:05:47 +0200

    Add file with constants

---
+++ src/main/java/pl/roquv/passwordgeneratorjavafx/Constants.java
@@ -0,0 +1,8 @@
+package pl.roquv.passwordgeneratorjavafx;
+
+public class Constants {
+    public static final String LOWERCASE_CHARACTERS = "abcdefghijklmnoprstuvwxyz";
+    public static final String UPPERCASE_CHARACTERS = "ABCDEFGHIJKLMOPRSTUVWXYZ";
+    public static final String NUMBERS = "0123456789";
+    public static final String SPECIAL_SYMBOLS = "!@#$%^&*()-_=+[]{};:,.<>/?";
+}
