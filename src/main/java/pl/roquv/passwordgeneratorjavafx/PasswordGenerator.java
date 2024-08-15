84e77be78d65ef5d4ae99628745c07ee1e39a6bb
Roquv13 <czarekolszewski99@gmail.com>
2024-08-15 23:29:15 +0200

    Add Password Generator to separate file

--- src/main/java/pl/roquv/passwordgeneratorjavafx/PasswordGenerator.java
+++ src/main/java/pl/roquv/passwordgeneratorjavafx/PasswordGenerator.java
@@ -1,31 +1,8 @@
 package pl.roquv.passwordgeneratorjavafx;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.*;
-
 import java.util.Random;
 
 public class PasswordGenerator {
-    @FXML
-    private TextField passwordLabel;
-
-    @FXML
-    private TextField passwordLengthLabel;
-
-    @FXML
-    private Button generateButton;
-
-    @FXML
-    private CheckBox checkBoxLowercase;
-
-    @FXML
-    private CheckBox checkBoxUppercase;
-
-    @FXML
-    private CheckBox checkBoxNumbers;
-
-    @FXML
-    private CheckBox checkBoxSpecialCharacters;
 
     private final Random random;
 
