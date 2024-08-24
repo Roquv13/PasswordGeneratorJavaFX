b7d77b5f1445c60a9f56ae7e0ce456b09fb6920e
Roquv13 <czarekolszewski99@gmail.com>
2024-08-24 19:19:28 +0200

    Add choiceBox for language set up

--- src/main/java/pl/roquv/passwordgeneratorjavafx/Controllers/SettingsWindowController.java
+++ src/main/java/pl/roquv/passwordgeneratorjavafx/Controllers/SettingsWindowController.java
@@ -1,5 +1,33 @@
 package pl.roquv.passwordgeneratorjavafx.Controllers;
 
-public class SettingsWindowController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ChoiceBox;
 
+public class SettingsWindowController {
+    @FXML
+    private ChoiceBox<String> choiceBoxLanguage;
+
+    @FXML
+    private void initialize() {
+        initializeChoiceBoxLanguage();
+    }
+
+    private void initializeChoiceBoxLanguage() {
+        choiceBoxLanguage.getItems().addAll("English", "Polish");
+
+        choiceBoxLanguage.setValue("English");
+
+        choiceBoxLanguage.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            handleChoiceBoxSelection(newValue);
+        });
+    }
+
+    private void handleChoiceBoxSelection(String selectedItem) {
+        if (selectedItem.equals("English")) {
+            System.out.println("Selected Language: " + choiceBoxLanguage.getValue());
+        } else if (selectedItem.equals("Polish")) {
+            System.out.println("Selected Language: " + choiceBoxLanguage.getValue());
+        }
+    }
 }
